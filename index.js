f0b9907ba4e851139bb6e69609f66f85bccc7a0f
Nonis17 <emma.lilja@student.kyh.se>
2020-09-14 17:06:58 +0200

    Latest update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-/*JavaScript som får en popup ruta att dyka upp och försvinna*/
 
+/*Deklarerar variabler i JS kopplat till html*/
 var pButton = document.querySelector('.button'); 
 var buttonBg = document.querySelector('.button-bg');
 var xKnapp = document.querySelector('.x-close'); 
@@ -8,7 +8,13 @@ var tack = document.querySelector('.tack');
 var reply = document.querySelector('.reply');
 var skickaknapp = document.querySelector('.knapp');
 var textruta = document.querySelector('.bio');
+var epostInput = document.querySelector('.email');
+var nameInput = document.querySelector('.name');
+/*var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;*/
 
+
+
+/*JavaScript som får en popup ruta att dyka upp och försvinna när man trycker på prenumereraknappen*/
 pButton.addEventListener('click', function() {
     buttonBg.classList.add('bg-active');
 });
@@ -17,22 +23,38 @@ xKnapp.addEventListener('click', function() {
     buttonBg.classList.remove('bg-active');
 });
 
+/*Tack-texten dyker upp när man fyllt i epostadressen i prenumerationen*/
 popUpButton.addEventListener('click', function() {
     popUpButton.style.cssText = "background-color: lightgrey;"; 
+   tack.classList.add('tack-active'); 
+    
+    if (nameInput.value.length > 0 && epostInput.value.length > 0) {
+        tack.textContent = "Tack!"; 
+    }
+    
+    else{
+        tack.style.color = "#9f0000";
+        tack.textContent = "Fyll i de tomma fältet"; 
+    }
+      
 
-    tack.classList.add('tack-active');
 });
 
-/*När man trycker på skicka-knappen får man ett meddelande*/
+/*När man trycker på skicka-knappen i kontakt-formuläret får man ett meddelande*/
 skickaknapp.addEventListener('click', function() {
     reply.classList.add('reply-active');
 
-    if (textruta.value.length > 0) {
+    if (textruta.value.length > 0 && epostInput.value.length > 0) {
         reply.style.color = "black";
-        reply.textContent = "Tack! Jag svarar så fort jag kan"; 
+        reply.textContent = "Tack! Vi svarar så fort vi kan"; 
     }
-    else {
+    else if (textruta.value.length > 0 ) {
+        reply.style.color = "#9f0000";
+        reply.textContent = "Felaktig e-postadress"; 
+    }
+    else{
         reply.style.color = "#9f0000";
         reply.textContent = "Fyll i de tomma fältet"; 
     }
 });
+
