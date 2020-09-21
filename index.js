5472974bf21c0b4aa7f2e8756c09f90d850cc641
Nonis17 <emma.lilja@student.kyh.se>
2020-09-21 14:20:09 +0200

    Latest updates

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 /*Deklarerar variabler i JS kopplat till html*/
 var pButton = document.querySelector('.button'); 
 var buttonBg = document.querySelector('.button-bg');
-var xKnapp = document.querySelector('.x-close'); 
+var xButton = document.querySelector('.x-close'); 
 var popUpButton = document.querySelector('.popUpButton');
 var thankYou = document.querySelector('.thankYou'); 
 var reply = document.querySelector('.reply');
 var sendButton = document.querySelector('.sendButton');
 var textBio = document.querySelector('.bio');
-var nameInput = document.querySelector('.name');
+var nameInput = document.querySelector('#prenumerationName');
 var emailContact = document.querySelector('#contactEmail');
 var emailPren = document.querySelector('#prenumerationEmail');
+var inputForm = document.querySelector('.inputForm');
 
 /*Den animerade menyn*/
 const hamburger = document.querySelector('.hamburger'); 
@@ -25,12 +26,12 @@ hamburger.addEventListener("click", () => {
 })
 
 
-/*Funktion: JavaScript som får en popup ruta att dyka upp och försvinna när man trycker på prenumereraknappen*/
+/*Funktion: JavaScript som får en popup ruta att dyka upp och försvinna*/
 pButton.addEventListener('click', function() {
     buttonBg.classList.add('bg-active');
 });
 
-xKnapp.addEventListener('click', function() {
+xButton.addEventListener('click', function() {
     buttonBg.classList.remove('bg-active');
 });
 
@@ -39,14 +40,26 @@ popUpButton.addEventListener('click', function() {
     popUpButton.style.cssText = "background-color: lightgrey;"; 
     thankYou.classList.add('thankYou-active'); 
   
-      if (nameInput.value.length > 0 && emailPren.value.length > 0) {
+      if (nameInput.value.length > 1 && emailPren.value.length > 0) {
         thankYou.style.color = "black";
         thankYou.textContent = "Tack!"; 
     }
-    
-    else{
+    else if (nameInput.value.length > 1 && emailPren.value.length < 1) {
         thankYou.style.color = "#9f0000";
-        thankYou.textContent = "Fyll i de tomma fältet"; 
+        thankYou.textContent = "Fyll i e-postadressen"; 
+        nameInput.style.css = "border: 1px solid red;";
+    }
+
+    else if (nameInput.value.length < 1 && emailPren.value.length < 1) {
+        thankYou.style.color = "#9f0000";
+        thankYou.textContent = "Fyll i namn och e-postadress"; 
+        nameInput.style.css = "border: 1px solid red;";
+    }
+  
+    else{
+        thankYou.style.color = "red";
+        thankYou.textContent = "Fyll i namn"; 
+
     }
 });
 
@@ -54,7 +67,7 @@ popUpButton.addEventListener('click', function() {
 sendButton.addEventListener('click', function() {
     reply.classList.add('reply-active');
 
-    if (textBio.value.length > 0 && emailContact.value.length > 0 && nameInput.value.length > 0) {
+    if (textBio.value.length > 0 && emailContact.value.length > 0 && nameInput.value.length > 1) {
         reply.style.color = "black";
         reply.textContent = "Tack! Vi svarar så fort vi kan"; 
     }
