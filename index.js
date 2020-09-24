57b83d8659a9fd36dbba8cba1d4b202deddf81f3
Nonis17 <emma.lilja@student.kyh.se>
2020-09-24 10:09:39 +0200

    latest updates

--- index.js
+++ index.js
@@ -11,19 +11,6 @@ var textBio = document.querySelector('.bio');
 var nameInput = document.querySelector('#prenumerationName');
 var emailContact = document.querySelector('#contactEmail');
 var emailPren = document.querySelector('#prenumerationEmail');
-var inputForm = document.querySelector('.inputForm');
-
-/*Den animerade menyn*/
-const hamburger = document.querySelector('.hamburger'); 
-const navLinks = document.querySelector('.nav-links'); 
-const links = document.querySelectorAll('.nav-links li'); 
-
-hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("fade"); 
-    });
-})
 
 
 /*Funktion: JavaScript som får en popup ruta att dyka upp och försvinna*/
@@ -35,32 +22,101 @@ xButton.addEventListener('click', function() {
     buttonBg.classList.remove('bg-active');
 });
 
+/*Här försöker jag göra ny kod till formulärsvalideringen*/
+
+
+const form  = document.getElementsByTagName('form')[0];
+
+const email = document.getElementById('prenumerationEmail');
+const emailError = document.querySelector('#prenumerationEmail + span.thankYou');
+
+email.addEventListener('input', function (event) {  
+  // Each time the user types something, we check if the
+  // form fields are valid.
+  nameInput.style.borderColor = "white";
+        emailPren.style.borderColor = "white";
+
+  if (email.validity.valid) {
+    
+    /*thankYou.classList.add('thankYou-active'); 
+    thankYou.style.color = "black";
+    thankYou.textContent = "Tack!";*/
+    emailPren.style.borderColor = "white";
+    prenButton();
+    // In case there is an error message visible, if the field
+    // is valid, we remove the error message.
+
+  } else {
+    // If there is still an error, show the correct error
+    showError();
+  }
+});
+
+form.addEventListener('submit', function (event) {
+  // if the email field is valid, we let the form submit
+
+  if(!email.validity.valid) {
+    // If it isn't, we display an appropriate error message
+    showError();
+    // Then we prevent the form from being sent by canceling the event
+    event.preventDefault();
+  }
+});
+
+function showError() {
+  if(email.validity.valueMissing) {
+    // If the field is empty
+    // display the following error message.
+    emailPren.style.borderColor = "red";
+    thankYou.style.color = "red";
+    thankYou.textContent = 'Du behöver skriva en e-postadress!';
+    nameInput.style.borderColor = "white";
+  } else if(email.validity.typeMismatch) {
+    // If the field doesn't contain an email address
+    // display the following error message.
+    thankYou.textContent = 'Det måste vara en e-postadress';
+  } 
+
+  // Set the styling appropriately
+ thankYou.className = '.thankYou-active';
+}
+
+
+
 /*Funktion: Tack-texten dyker upp när man fyllt i epostadressen i prenumerationen*/
-popUpButton.addEventListener('click', function() {
-    popUpButton.style.cssText = "background-color: lightgrey;"; 
-    thankYou.classList.add('thankYou-active'); 
+popUpButton.addEventListener('click', function prenButton() {
+    thankYou.classList.add('thankYou-active');
   
       if (nameInput.value.length > 1 && emailPren.value.length > 0) {
         thankYou.style.color = "black";
         thankYou.textContent = "Tack!"; 
+        nameInput.style.borderColor = "white";
+        emailPren.style.borderColor = "white";
+        
     }
-    else if (nameInput.value.length > 1 && emailPren.value.length < 1) {
-        thankYou.style.color = "#9f0000";
+    if (nameInput.value.length > 1 && emailPren.value.length < 1) {
+        thankYou.style.color = "red";
         thankYou.textContent = "Fyll i e-postadressen"; 
-        nameInput.style.css = "border: 1px solid red;";
+        emailPren.style.borderColor = "red";
+        nameInput.style.borderColor = "white";
+        showError();
+        
     }
 
     else if (nameInput.value.length < 1 && emailPren.value.length < 1) {
-        thankYou.style.color = "#9f0000";
+        thankYou.style.color = "red";
         thankYou.textContent = "Fyll i namn och e-postadress"; 
-        nameInput.style.css = "border: 1px solid red;";
+        nameInput.style.borderColor = "red";
+        emailPren.style.borderColor = "red";
     }
-  
-    else{
+
+    else if (nameInput.value.length < 2) {
         thankYou.style.color = "red";
         thankYou.textContent = "Fyll i namn"; 
-
+        nameInput.style.borderColor = "red";
+    
     }
+  
 });
 
 /*Funktion: När man trycker på skicka-knappen i kontakt-formuläret får man ett meddelande*/
